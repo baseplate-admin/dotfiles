6a33812fd30df3bd2b3fb8bde0a2085fd5261506
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-01-12 11:12:47 +0600

    Update apply.py

--- apply.py
+++ apply.py
@@ -9,35 +9,47 @@ if not sys.platform == "linux":
     print("This code can be only executed from linux")
     sys.exit(0)
 
-CURRENT_DIRECTORY = os.path.realpath(os.path.dirname(__file__))
 
-# Apply font files
-FONT_DIRECTORY = pathlib.Path(CURRENT_DIRECTORY, "fonts")
-for file in FONT_DIRECTORY.glob("*py"):
-    subprocess.call(["python", file])
+def main():
+    CURRENT_DIRECTORY = os.path.realpath(os.path.dirname(__file__))
 
+    # Apply font files
+    FONT_DIRECTORY = pathlib.Path(CURRENT_DIRECTORY, "fonts")
+    for file in FONT_DIRECTORY.glob("*py"):
+        subprocess.call(["python", file])
 
-# Work with config directory
-CONFIG_DIRECTORY = pathlib.Path(CURRENT_DIRECTORY, ".config")
-for file in CONFIG_DIRECTORY.glob("**/*"):
-    if not file.is_file():
-        continue
-
-    new_config_file = file.relative_to(CURRENT_DIRECTORY)
-    old_config_file = pathlib.Path("/home", os.getlogin(), new_config_file)
-
-    if os.path.isfile(old_config_file):
-        ask = input(
-            f"Old config file for {new_config_file}. Do you want to replace?"
-        ).lower()
-        if "n" in ask:
+    # Work with config directory
+    CONFIG_DIRECTORY = pathlib.Path(CURRENT_DIRECTORY, ".config")
+    for file in CONFIG_DIRECTORY.glob("**/*"):
+        if not file.is_file():
             continue
 
-    if not os.path.exists(os.path.dirname(old_config_file)):
-        os.makedirs(os.path.dirname(old_config_file), exist_ok=True)
-        shutil.chown(
-            os.path.dirname(old_config_file), user=os.getlogin(), group=os.getlogin()
-        )
+        new_config_file = file.relative_to(CURRENT_DIRECTORY)
+        old_config_file = pathlib.Path("/home", os.getlogin(), new_config_file)
 
-    shutil.copy(src=new_config_file, dst=old_config_file)
-    shutil.chown(old_config_file, user=os.getlogin(), group=os.getlogin())
+        if os.path.isfile(old_config_file):
+            ask = input(
+                f"Old config file for {new_config_file}. Do you want to replace?"
+            ).lower()
+            if "n" in ask:
+                continue
+
+        if not os.path.exists(os.path.dirname(old_config_file)):
+            os.makedirs(os.path.dirname(old_config_file), exist_ok=True)
+            shutil.chown(
+                os.path.dirname(old_config_file),
+                user=os.getlogin(),
+                group=os.getlogin(),
+            )
+
+        shutil.copy(src=new_config_file, dst=old_config_file)
+        shutil.chown(old_config_file, user=os.getlogin(), group=os.getlogin())
+
+
+if __name__ == "__main__":
+    if os.geteuid() == 0:
+        main()
+
+    else:
+        print("You need root permission for this script to function properly")
+        subprocess.check_call(["sudo", sys.executable] + sys.argv)
